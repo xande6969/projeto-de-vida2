36d47052f13d0bbf7cc85685288e7a19316c3b36
xande6969 <alexandre.medeiros.domingues@escola.pr.gov.br>
2026-05-07 12:37:23 -0300

    codigo de seleção

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const botoes = document.querySelectorAll('.botao')
 
 for(let botao = 0; botao < 4; botao++){
    botoes[botao].onclick = function(){
+
+      for(let outro = 0; outro < 4; outro++){
+         botoes[outro].classList.remove('ativo')
+      }
+
       botoes[botao].classList.add('ativo')
    }
 }
